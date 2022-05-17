8d821daddfc8b1273bb2d448092de75190750181
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-05-17 18:45:19 +0530

    updated card logs

--- day1-intro/src/components/ContactCard.jsx
+++ day1-intro/src/components/ContactCard.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import card from "./module.css";
 
 const ContactCard = ({ id, profilePic, name, email, phone, onHide }) => {
+  // const [contactCard, toggleCard] = UseCardToggle()
+  const [toggle, setToggle] = useState(false);
   return (
-    <div
-      style={{
-        display: "flex-box",
-        padding: "1rem",
-        border: "1px solid black",
-        marginBottom: "0.25rem",
-        gap: "1rem",
-      }}
-    >
-      <div>
-        {profilePic} {" "} {name} <br /> {"  "} {email} <br /> {" "} {phone}
+    <div>
+      <div
+        style={{
+          display: "flex-box",
+          padding: "1rem",
+          border: "1px solid black",
+          marginBottom: "0.25rem",
+          gap: "1rem",
+        }}
+      >
+        <div className={card} onClick={() => setToggle(!toggle)}> Click to show details
+          <div className={profilePic}>{profilePic}</div>
+          <div>
+            <div className={name}>{name}</div>
+
+            <div className={email}>{email}</div>
+            {toggle ? <div className={phone}>{phone}</div> : ""}
+          </div>
+        </div>
       </div>
-      <button onClick={() => onHide(id)}>Delete</button>
     </div>
   );
 };
